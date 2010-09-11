898323147705f6213094d26bf451d371f5577092
Pascal Hartig (Home) <phartig@rdrei.net>
2010-09-11 15:27:32 +0200

    Libraries in cmake file

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+#include <glib.h>
+#include <dbus/dbus-glib.h>
+
 
 int main(int argc, char *argv[]) {
     (void) argc;
     (void) argv;
+
+    GMainLoop *loop;
+    loop = g_main_loop_new(NULL, false);
+
+    g_main_loop_run(loop);
     return EXIT_SUCCESS;
 }
