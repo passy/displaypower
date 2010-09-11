6aee6bfc7b56e5a494ee3a4d46cdf1af73e11a0b
Pascal Hartig (Home) <phartig@rdrei.net>
2010-09-11 17:11:33 +0200

    Enabled DPMS on event

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #include <glib.h>
 #include <dbus/dbus-glib.h>
+
+
+void gdp_enable_dpms_suspend(void) {
+    system("xset dpms force standby");
+    printf("Activated DPMS.\n");
+}
 
 
 static DBusGConnection *setup_dbus() {
@@ -12,12 +19,37 @@ static DBusGConnection *setup_dbus() {
 
     bus = dbus_g_bus_get(DBUS_BUS_SESSION, &error);
     if (!bus) {
-        g_warning("Failed to connection to DBus session daemon: %s", error->message);
+        g_warning("Failed to connection to DBus session daemon: %s",
+                error->message);
         g_error_free(error);
         return NULL;
     }
 
     return bus;
+}
+
+
+static void on_screensaver_active_changed(DBusGProxy *proxy,
+        bool active) {
+
+    // Suppress unused parameter warning.
+    (void) proxy;
+    if (active) {
+        gdp_enable_dpms_suspend();
+    }
+}
+
+
+static DBusGProxy *setup_dbus_proxy(DBusGConnection *bus) {
+    DBusGProxy *proxy;
+
+    proxy = dbus_g_proxy_new_for_name(bus, "org.gnome.ScreenSaver",
+            "/org/gnome/ScreenSaver", "org.gnome.ScreenSaver");
+    dbus_g_proxy_add_signal(proxy, "ActiveChanged", G_TYPE_BOOLEAN);
+    dbus_g_proxy_connect_signal(proxy, "ActiveChanged",
+            G_CALLBACK(on_screensaver_active_changed), NULL, NULL);
+
+    return proxy;
 }
 
 
@@ -27,6 +59,7 @@ int main(int argc, char *argv[]) {
 
     GMainLoop *loop;
     DBusGConnection *bus;
+    DBusGProxy *proxy;
 
     // Must be called prior to any object access. Initializes the thread system,
     // as well.
@@ -34,11 +67,16 @@ int main(int argc, char *argv[]) {
 
     loop = g_main_loop_new(NULL, false);
     bus = setup_dbus();
+    proxy = setup_dbus_proxy(bus);
     if (bus == NULL) {
         return EXIT_FAILURE;
     }
 
     g_main_loop_run(loop);
+    g_main_loop_unref(loop);
+
+    g_object_unref(bus);
+    g_object_unref(proxy);
 
     return EXIT_SUCCESS;
 }
